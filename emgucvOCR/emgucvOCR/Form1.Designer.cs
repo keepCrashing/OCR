4b087fa1f82bf17bd1e86493f0e14ad20008396d
keepCrashing <deer.of.nine.colors@gmail.com>
2017-12-04 04:50:59 +0800

    Add files via upload

---
+++ emgucvOCR/emgucvOCR/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace emgucvOCR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.btnOtsu = new System.Windows.Forms.Button();
+            this.btnGray = new System.Windows.Forms.Button();
+            this.btnSobel = new System.Windows.Forms.Button();
+            this.btnCanny = new System.Windows.Forms.Button();
+            this.btnOcr = new System.Windows.Forms.Button();
+            this.txtOcr = new System.Windows.Forms.TextBox();
+            this.btnMedianBlur = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
+            this.imageBox1.Location = new System.Drawing.Point(12, 39);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(640, 480);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.imageBox1_Paint);
+            this.imageBox1.DoubleClick += new System.EventHandler(this.imageBox1_DoubleClick);
+            this.imageBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.imageBox1_MouseDown);
+            this.imageBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.imageBox1_MouseMove);
+            this.imageBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.imageBox1_MouseUp);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 546);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
+            this.imageBox2.Location = new System.Drawing.Point(668, 39);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(640, 480);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // btnOtsu
+            // 
+            this.btnOtsu.Location = new System.Drawing.Point(749, 558);
+            this.btnOtsu.Name = "btnOtsu";
+            this.btnOtsu.Size = new System.Drawing.Size(75, 23);
+            this.btnOtsu.TabIndex = 4;
+            this.btnOtsu.Text = "Otsu";
+            this.btnOtsu.UseVisualStyleBackColor = true;
+            this.btnOtsu.Click += new System.EventHandler(this.btnOtsu_Click);
+            // 
+            // btnGray
+            // 
+            this.btnGray.Location = new System.Drawing.Point(668, 558);
+            this.btnGray.Name = "btnGray";
+            this.btnGray.Size = new System.Drawing.Size(75, 23);
+            this.btnGray.TabIndex = 5;
+            this.btnGray.Text = "Gray";
+            this.btnGray.UseVisualStyleBackColor = true;
+            this.btnGray.Click += new System.EventHandler(this.btnGray_Click);
+            // 
+            // btnSobel
+            // 
+            this.btnSobel.Location = new System.Drawing.Point(831, 557);
+            this.btnSobel.Name = "btnSobel";
+            this.btnSobel.Size = new System.Drawing.Size(75, 23);
+            this.btnSobel.TabIndex = 6;
+            this.btnSobel.Text = "Sobel";
+            this.btnSobel.UseVisualStyleBackColor = true;
+            this.btnSobel.Click += new System.EventHandler(this.btnSobel_Click);
+            // 
+            // btnCanny
+            // 
+            this.btnCanny.Location = new System.Drawing.Point(913, 557);
+            this.btnCanny.Name = "btnCanny";
+            this.btnCanny.Size = new System.Drawing.Size(75, 23);
+            this.btnCanny.TabIndex = 7;
+            this.btnCanny.Text = "Canny";
+            this.btnCanny.UseVisualStyleBackColor = true;
+            this.btnCanny.Click += new System.EventHandler(this.btnCanny_Click);
+            // 
+            // btnOcr
+            // 
+            this.btnOcr.Location = new System.Drawing.Point(668, 604);
+            this.btnOcr.Name = "btnOcr";
+            this.btnOcr.Size = new System.Drawing.Size(75, 23);
+            this.btnOcr.TabIndex = 8;
+            this.btnOcr.Text = "Ocr";
+            this.btnOcr.UseVisualStyleBackColor = true;
+            this.btnOcr.Click += new System.EventHandler(this.btnOcr_Click);
+            // 
+            // txtOcr
+            // 
+            this.txtOcr.Location = new System.Drawing.Point(668, 665);
+            this.txtOcr.Multiline = true;
+            this.txtOcr.Name = "txtOcr";
+            this.txtOcr.ReadOnly = true;
+            this.txtOcr.Size = new System.Drawing.Size(536, 81);
+            this.txtOcr.TabIndex = 9;
+            // 
+            // btnMedianBlur
+            // 
+            this.btnMedianBlur.Location = new System.Drawing.Point(994, 558);
+            this.btnMedianBlur.Name = "btnMedianBlur";
+            this.btnMedianBlur.Size = new System.Drawing.Size(106, 23);
+            this.btnMedianBlur.TabIndex = 10;
+            this.btnMedianBlur.Text = "medianBlur";
+            this.btnMedianBlur.UseVisualStyleBackColor = true;
+            this.btnMedianBlur.Click += new System.EventHandler(this.btnMedianBlur_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1310, 776);
+            this.Controls.Add(this.btnMedianBlur);
+            this.Controls.Add(this.txtOcr);
+            this.Controls.Add(this.btnOcr);
+            this.Controls.Add(this.btnCanny);
+            this.Controls.Add(this.btnSobel);
+            this.Controls.Add(this.btnGray);
+            this.Controls.Add(this.btnOtsu);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button btnStart;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button btnOtsu;
+        private System.Windows.Forms.Button btnGray;
+        private System.Windows.Forms.Button btnSobel;
+        private System.Windows.Forms.Button btnCanny;
+        private System.Windows.Forms.Button btnOcr;
+        private System.Windows.Forms.TextBox txtOcr;
+        private System.Windows.Forms.Button btnMedianBlur;
+    }
+}
+
---
+++ emgucvOCR/emgucvOCR/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+using Emgu.CV.OCR;
+namespace emgucvOCR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private VideoCapture capture;
+        private bool isProcess = false;
+        private bool isOstu = false;
+        private bool isGray = false;
+        private bool isSobel = false;
+        private bool isCanny = false;
+        private bool isOcr = false;
+        private bool isMedianBlur = false;
+        private Rectangle Rect = new Rectangle();
+        private Point RectStartPoint;
+        Mat src;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                capture = new VideoCapture();
+                if (capture != null)
+                {
+                    if (isProcess)
+                    {
+                        Application.Idle -= new EventHandler(ProcessFrame);
+                        this.btnStart.Text = "Start";
+                    }
+                    else
+                    {
+                        Application.Idle += new EventHandler(ProcessFrame);
+                        this.btnStart.Text = "Stop";
+                    }
+                    isProcess = !isProcess;
+                }
+            }
+            catch (NullReferenceException expt)
+            {
+                MessageBox.Show(expt.Message);
+            }
+        }
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            Mat src = capture.QueryFrame();
+            imageBox1.Image = src;
+            if(isOstu == true)
+            {
+                Size sz = src.Size;
+                Mat[] mat = src.Split();
+                Mat dst = new Mat(sz, DepthType.Cv8U, 1);
+                CvInvoke.CvtColor(src, dst, ColorConversion.Bgr2Gray);
+                CvInvoke.Threshold(dst, dst, 0, 255, ThresholdType.Binary | ThresholdType.Otsu);
+                imageBox2.Image = dst;
+
+            }
+            else if (isGray == true)
+            {
+                Size sz = src.Size;
+                Mat[] mat = src.Split();
+                Mat dst = new Mat(sz, DepthType.Cv8U, 1);
+                CvInvoke.CvtColor(src, dst, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                //CvInvoke.Threshold(dst, dst, 0, 255, ThresholdType.Binary | ThresholdType.Otsu);
+                imageBox2.Image = dst;
+
+            }
+            else if(isSobel == true)
+            {
+                int scale = 1;
+                int delta = 0;
+                Size sz = src.Size;
+                Mat[] mat = src.Split();
+                Mat gray = new Mat(sz, DepthType.Cv8U, 1);
+                Mat grad = new Mat(sz, DepthType.Cv8U, 1);
+                Mat grad_x = new Mat(sz, DepthType.Cv8U, 1), grad_y = new Mat(sz, DepthType.Cv8U, 1);
+                Mat abs_grad_x = new Mat(sz, DepthType.Cv8U, 1), abs_grad_y = new Mat(sz, DepthType.Cv8U, 1);
+                CvInvoke.CvtColor(src, gray, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                CvInvoke.Sobel(gray, grad_x, Emgu.CV.CvEnum.DepthType.Cv16S, 1, 0, 3, scale, delta, Emgu.CV.CvEnum.BorderType.Default);
+                CvInvoke.ConvertScaleAbs(grad_x, abs_grad_x, 1.0, 0.0);
+                CvInvoke.Sobel(gray, grad_y, Emgu.CV.CvEnum.DepthType.Cv16S, 0, 1, 3, scale, delta, Emgu.CV.CvEnum.BorderType.Default);
+                CvInvoke.ConvertScaleAbs(grad_y, abs_grad_y, 1.0, 0.0);
+                CvInvoke.AddWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+                imageBox2.Image = grad;
+            }
+            else if (isCanny == true)
+            {
+                Size sz = src.Size;
+                Mat[] mat = src.Split();
+                Mat dst = new Mat(sz, DepthType.Cv8U, 1);
+                CvInvoke.CvtColor(src, dst, ColorConversion.Bgr2Gray);
+                CvInvoke.Canny(dst, dst, 50, 150, 3);
+                //CvInvoke.Threshold(dst, dst, 0, 255, ThresholdType.Binary | ThresholdType.Otsu);
+                imageBox2.Image = dst;
+            }else if(isMedianBlur == true)
+            {
+                Size sz = src.Size;
+                Mat[] mat = src.Split();
+                Mat dst = new Mat(sz, DepthType.Cv8U, 3);
+                CvInvoke.MedianBlur(src, dst, 5);
+                imageBox2.Image = dst;
+            }
+            else if(isOcr == true)
+            {
+                Image<Bgr, byte> img;
+                img = new Image<Bgr, byte>(imageBox1.Image.Bitmap);
+                //imgEntrada.Draw(Rect, new Bgr(Color.Red), 3);
+                img.ROI = Rect;
+                Tesseract tess;
+                //tess = new Tesseract("tessdata", "eng", Tesseract.OcrEngineMode.OEM_TESSERACT_CUBE_COMBINED);
+                tess = new Tesseract(@"tessdata\", "eng", OcrEngineMode.TesseractLstmCombined);
+                tess.SetVariable("tessedit_char_whitelist", "1234567890");
+                Tesseract.Character[] words;
+                Mat dst = new Mat(img.Size, DepthType.Cv8U, 1);
+                CvInvoke.CvtColor(img.Mat, dst, ColorConversion.Bgr2Gray);
+                //CvInvoke.Threshold(dst, dst, 0, 255, ThresholdType.Binary | ThresholdType.Otsu);
+                CvInvoke.Canny(dst, dst, 50, 150, 3);
+                //threshold(dst1, dst2, 128, 255, THRESH_BINARY_INV);
+                CvInvoke.Threshold(dst, dst, 128, 255, ThresholdType.BinaryInv);
+                tess.SetImage(dst);
+                tess.Recognize();
+                words = tess.GetCharacters();
+                imageBox2.Image = dst;
+                txtOcr.Text = tess.GetUTF8Text();
+            }
+            else
+            {
+                imageBox2.Image = src;
+            }
+
+        }
+        private void btnOtsu_Click(object sender, EventArgs e)
+        {
+            isOstu = true;
+            isGray = false;
+            isSobel = false;
+            isCanny = false;
+            isMedianBlur = false;
+
+            isOcr = false;
+        }
+
+        private void btnGray_Click(object sender, EventArgs e)
+        {
+            isGray = true;
+            isOstu = false;
+            isSobel = false;
+            isCanny = false;
+            isMedianBlur = false;
+
+            isOcr = false;
+        }
+
+        private void btnSobel_Click(object sender, EventArgs e)
+        {
+            isSobel = true;
+            isGray = false;
+            isOstu = false;
+            isCanny = false;
+            isMedianBlur = false;
+
+            isOcr = false;
+        }
+
+        private void btnCanny_Click(object sender, EventArgs e)
+        {
+            isCanny = true;
+            isSobel = false;
+            isGray = false;
+            isOstu = false;
+            isMedianBlur = false;
+
+            isOcr = false;
+        }
+
+        private void imageBox1_DoubleClick(object sender, EventArgs e)
+        {
+            Rect = new Rectangle();
+            ((PictureBox)sender).Invalidate();
+        }
+
+        private void imageBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if(imageBox1 != null)
+            {
+                if (Rect != null && Rect.Width > 0 && Rect.Height > 0)
+                {
+                    e.Graphics.SetClip(Rect, System.Drawing.Drawing2D.CombineMode.Exclude);
+                    e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(128, 64, 64, 64)), new Rectangle(0, 0, ((PictureBox)sender).Width, ((PictureBox)sender).Height));
+                }
+
+            }
+        }
+
+        private void imageBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            RectStartPoint = e.Location;
+            Invalidate();
+        }
+
+        private void imageBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void imageBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+            Point tempEndPoint = e.Location;
+            Rect.Location = new Point(
+                Math.Min(RectStartPoint.X, tempEndPoint.X),
+                Math.Min(RectStartPoint.Y, tempEndPoint.Y));
+            Rect.Size = new Size(
+                Math.Abs(RectStartPoint.X - tempEndPoint.X),
+                Math.Abs(RectStartPoint.Y - tempEndPoint.Y));
+        }
+
+        private void btnOcr_Click(object sender, EventArgs e)
+        {
+            isOcr = true;
+            isCanny = false;
+            isSobel = false;
+            isGray = false;
+            isOstu = false;
+            isMedianBlur = false;
+        }
+
+        private void btnMedianBlur_Click(object sender, EventArgs e)
+        {
+            isMedianBlur = true;
+
+            isOcr = false;
+            isCanny = false;
+            isSobel = false;
+            isGray = false;
+            isOstu = false;
+        }
+    }
+}
